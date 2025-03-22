f5963f789a985b734951c85ac7b5aa0e0c6875f9
pyguydotpy <pydevtesting22@gmail.com>
2025-03-22 16:38:14 -0400

    removed unwanted file

--- platformscript.py
+++
@@ -1,8 +0,0 @@
-import platform
-
-print("System:", platform.system())
-print("Node Name:", platform.node())
-print("Release:", platform.release())
-print("Version:", platform.version())
-print("Machine:", platform.machine())
-print("Processor:", platform.processor())
